6d07e2d5ae03be62e9b6cce0a9a7bccaeded6f8c
Matt Heard <matt.heard@powershop.co.nz>
2016-12-20 10:21:47 +1300

    Change indentation

--- test/test.js
+++ test/test.js
@@ -1,24 +1,23 @@
 QUnit.test("Tamagotchi type is defined", function(assert) {
-  assert.ok(t.Tamagotchi);
+    assert.ok(t.Tamagotchi);
 });
 
 QUnit.test("Tamagotchi can be instantiated", function(assert) {
-  assert.ok(new t.Tamagotchi());
+    assert.ok(new t.Tamagotchi());
 });
 
 QUnit.test("The first screen is the main screen", function(assert) {
-  tamagotchi = new t.Tamagotchi();
-  assert.equal(tamagotchi.screen, "main");
+    tamagotchi = new t.Tamagotchi();
+    assert.equal(tamagotchi.screen, "main");
 });
 
 QUnit.test("A tamagotchi can be told something", function(assert) {
-  tamagotchi = new t.Tamagotchi();
-  assert.ok(tamagotchi.tell("hello"));
+    tamagotchi = new t.Tamagotchi();
+    assert.ok(tamagotchi.tell("hello"));
 });
 
-QUnit.test("Telling a tamagotchi 'are you hungry?' will open the hunger screen",
-        function(assert) {
-  tamagotchi = new t.Tamagotchi();
-  tamagotchi.tell("are you hungry?");
-  assert.equal(tamagotchi.screen, "hunger");
+QUnit.test("Telling a tamagotchi 'are you hungry?' will open the hunger screen", function(assert) {
+    tamagotchi = new t.Tamagotchi();
+    tamagotchi.tell("are you hungry?");
+    assert.equal(tamagotchi.screen, "hunger");
 });
