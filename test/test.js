391249afa4d2e6973f959af95e83c2cfa0b9e1e2
Matt Heard <matt.heard@powershop.co.nz>
2016-12-19 16:22:53 +1300

    Test Tamagotchi instantiation

--- test/test.js
+++ test/test.js
@@ -1,3 +1,7 @@
 QUnit.test("Tamagotchi type is defined", function(assert) {
   assert.ok(Tamagotchi, "Passed!");
 });
+
+QUnit.test("Tamagotchi can be instantiated", function(assert) {
+  assert.ok(new Tamagotchi(), "Passed!");
+});
